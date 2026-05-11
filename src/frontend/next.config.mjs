82402a0776b0bedea69217382a3116a9b823a2f7
ctthong18 <thongphil18@gmail.com>
2026-05-12 01:09:25 +0700

    fix version

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -1,8 +1,3 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
@@ -11,8 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  output: 'standalone',
-  outputFileTracingRoot: __dirname,
 }
 
 export default nextConfig
